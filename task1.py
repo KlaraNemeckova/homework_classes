e4774faba1f2ed55e756b93b4f84cb02e7adfdde
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-16 18:45:51 +0200

    hotovo

--- task1.py
+++ task1.py
@@ -2,11 +2,18 @@ class Device:
     def __init__(self, category):
         self.category = category
 
-    def turn_on(self, category):
-        pass
 
-    def turn_off(self, category):
-        pass
+    def description(self):
+        print("It is a type of {}".format(self.category))
+
+    def turn_on(self):
+        print("The device is switched on.")
+
+    def turn_off(self):
+        print("The device is switched off.")
+
+    def broken(self):
+        print("The device is broken.")
 
 
 class CoffeeMachine(Device):
@@ -16,25 +23,27 @@ class CoffeeMachine(Device):
         self.year = year
         self.color = color
 
-
     def informations(self, brand, year, color):
         print(f"Coffee machine - brand: {brand}, year of production: {year}, color: {color}")
 
 
 class Blender(Device):
-    def __init__(self, category, brand, year, wattage):
+    def __init__(self, category, brand, year, wattage, volume):
         super().__init__(category)
         self.brand = brand
         self.year = year
         self.wattage = wattage
-
-    def informations(self, brand, year, wattage):
-        print(f"Blender - brand: {brand}, year of production: {year}, wattage: {wattage}")
+        self.volume = volume
 
 
-class MeatGrinder:
-    def __init__(self, brand, year, wattage):
-        super().__init__(brand)
+    def informations(self, brand, year, wattage, volume):
+        print(f"Blender - brand: {brand}, year of production: {year}, wattage: {wattage}, volume: {volume}")
+
+
+class MeatGrinder(Device):
+    def __init__(self, category, brand, year, wattage):
+        super().__init__(category)
+        self.brand = brand
         self.year = year
         self.wattage = wattage
 
@@ -44,17 +53,23 @@ class MeatGrinder:
 
 d = Device("Kitchen Appliances")
 print(d.category)
+print()
+
 c = CoffeeMachine("Kitchen Appliances", "Bosch", "2015", "black & grey")
 c.informations("Bosch", "2015", "black & grey")
+c.turn_on()
+
+print()
+
+b = Blender("Kitchen Appliances", "Philips", "2019", "1000 W", "2 l")
+b.informations("Philips", "2019", "1000 W", "2 l")
+b.broken()
 
 
-# Blender - Philips, 1000 W, 2 l
-#Grinder - Tefal, 2200 W
+print()
 
+g = MeatGrinder("Kitchen Appliances", "Tefal", "2012", "2200 W")
+g.informations("Tefal", "2012", "2200 W")
+g.turn_off()
 
-
-
-
-
-
-
+g.description()
