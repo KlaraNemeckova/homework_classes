7eb44c8b9ad885c2e4cbe62f8d48055f712e2a34
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-17 10:10:01 +0200

    task2

---
+++ task2.py
@@ -0,0 +1,22 @@
+class Ship:
+    def __init__(self, type, length):
+        self.type = type
+        self.length = length
+
+    def starting(self):
+        pass
+
+    def landing(self):
+        pass
+    
+
+class Frigate(Ship):
+    pass
+
+
+class Destroyer(Ship):
+    pass
+
+
+class Cruiser(Ship):
+    pass
