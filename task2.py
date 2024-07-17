22ef2eae7e18a9f63f68518f84c0c53db93663c1
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-17 10:51:36 +0200

    seznam planet

--- task2.py
+++ task2.py
@@ -1,3 +1,11 @@
+import random
+
+planets = ["Abafar", "Ahch-To", "Anaxes", "Bespin", "Cantonica", "Castilon", "Cato Neimoidia", "Corellia", "Coruscant",
+           "Daiyu", "D'Qar", "Dagobah", "Dagobah", "Endor", "Exegol", "Felucia", "Hoth", "Iego", "Iridonia", "Jakku",
+           "Jedha", "Kamino", "Kashyyyk", "Kessel", "Malastare", "Mandalore", "Mapuzo", "Mon Cala", "Mustafar", "Naboo",
+           "Nevarro", "Ossus", "Pasaana", "Polis Massa", "Ryloth", "Saleucami", "Serenno", "Tatooine", "Umbara",
+           "Utapau", "Wobani", "Yavin", "Zygerria"]
+
 class Ship:
     def __init__(self, type, length):
         self.type = type
@@ -8,7 +16,8 @@ class Ship:
 
     def landing(self):
         pass
-    
+        # pokud stejné --> "change of plans, returns to ___"
+
 
 class Frigate(Ship):
     pass
@@ -20,3 +29,7 @@ class Destroyer(Ship):
 
 class Cruiser(Ship):
     pass
+
+
+# random name- Cruiser, Destroyer, Frigate
+# přidat další lodě
